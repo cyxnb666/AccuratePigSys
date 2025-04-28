b2fca29516890e3a9ae647e5ac5e6594af6e9277
chenyaoxi <yaoxi.chen@zhixunchelian.cn>
2025-04-28 10:52:02 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     },
     server: {
         host: true,
-        port: 8081,
+        port: 8080,
         cors: true,
         proxy: {
             '/baseURL': {
