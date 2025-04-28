41a0da819830b80c23ed3f66109f6b7a47df0cbe
chenyaoxi <yaoxi.chen@zhixunchelian.cn>
2025-04-28 11:24:38 +0800

    1

--- src/views/Login/api.ts
+++ src/views/Login/api.ts
@@ -1,1 +1,17 @@
 import request from '@/request/index'
+import { sessionManager } from '@/request/sessionManager';
+
+/**
+ * 获取验证码
+ * @returns {Promise<Blob>}
+ */
+export function getVerifyCode() {
+    return request({
+        url: '/auth/getVerifyCode',
+        method: 'get',
+        responseType: 'blob',
+        headers: {
+            'JSESSIONID': sessionManager.getSessionId()
+        }
+    })
+}
